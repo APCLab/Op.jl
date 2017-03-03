d471c6e8281dcc6c23ef3bd4135e962e53e4f3c3
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-03 20:54:08 +0800

    rename

--- src/Op.jl
+++ src/Op.jl
@@ -11,11 +11,11 @@ export C
 :param r: risk-free interest rate
 :param σ: volatility
 """
-C (S, L, T, r, σ) = S * N(D1(S, L, T, r, σ)) - L * e^(-r * T) * N(D2(S, L, T, r, σ))
+C(S, L, T, r, σ) = S * N(D1(S, L, T, r, σ)) - L * e^(-r * T) * N(D2(S, L, T, r, σ))
 
 D1(S, L, T, r, σ) = (log(S / L) + (r + 0.5 * σ^2) * T) / (σ * √T)
 D2(S, L, T, r, σ) = (log(S / L) + (r - 0.5 * σ^2) * T) / (σ * √T)
 
 N(x) = cdf(Normal(), x)
 
-end Op  # module
+end  # module

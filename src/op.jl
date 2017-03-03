a27fe0a41326d365a28aae41895c45fd806053c7
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-03 13:00:10 +0800

    init commit

---
+++ src/op.jl
@@ -0,0 +1,19 @@
+"""
+:param S: stock price
+:param L: strike price
+:param T: time to execute
+:param r: riskless interest rate
+"""
+function λ(S, L, T, r, σ)
+    ...
+end
+
+
+function D1()
+    ...
+end
+
+
+function D2()
+    ...
+end

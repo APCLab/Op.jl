2bfd192ac906e6669d44d64c4155e52cb337b797
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-03 20:31:52 +0800

    Implement C, D1, D2, N

--- src/op.jl
+++ src/op.jl
@@ -1,19 +1,22 @@
+module Op
+
+using Distributions: cdf, Normal
+
+export C
+
+
 """
 :param S: stock price
 :param L: strike price
 :param T: time to execute
-:param r: riskless interest rate
+:param r: risk-free interest rate
+:param σ: volatility
 """
-function λ(S, L, T, r, σ)
-    ...
-end
+C (S, L, T, r, σ) = S * N(D1(S, L, T, r, σ)) - L * e^(-r * T) * N(D2(S, L, T, r, σ))
 
+D1(S, L, T, r, σ) = (log(S / L) + (r + 0.5 * σ^2) * T) / (σ * √T)
+D2(S, L, T, r, σ) = (log(S / L) + (r - 0.5 * σ^2) * T) / (σ * √T)
 
-function D1()
-    ...
-end
+N(x) = cdf(Normal(), x)
 
-
-function D2()
-    ...
-end
+end Op  # module

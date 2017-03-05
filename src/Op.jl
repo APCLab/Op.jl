bf820e316c37f55af8778be26488b98e255eeedf
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-05 22:05:38 +0800

    Fix the risk-free rate of interest

--- src/Op.jl
+++ src/Op.jl
@@ -5,17 +5,17 @@ using Distributions: cdf, Normal
 export C
 
 """
-:param S: stock price
+:param S: stock price (Array)
 :param L: strike price
 :param T: time to execute
-:param r: risk-free interest rate
 :param σ: volatility
+:param r: risk-free rate of interest
 """
-C(S, L, T, r, σ) = S * N(D1(S, L, T, r, σ)) - L * e^(-r * T) * N(D2(S, L, T, r, σ))
+C(S, L, T, σ, r = 1.04)::Float64 = S * N(D1(S, L, T, r, σ)) - L * e^(-r * T) * N(D2(S, L, T, r, σ))
 
-D1(S, L, T, r, σ) = (log(S / L) + (r + 0.5 * σ^2) * T) / (σ * √T)
-D2(S, L, T, r, σ) = (log(S / L) + (r - 0.5 * σ^2) * T) / (σ * √T)
+D1(S, L, T, σ, r = 1.04)::Float64 = (log(S / L) + (r + 0.5 * σ^2) * T) / (σ * √T)
+D2(S, L, T, σ, r = 1.04)::Float64 = (log(S / L) + (r - 0.5 * σ^2) * T) / (σ * √T)
 
-N(x) = cdf(Normal(), x)
+N(x::Float64)::Float64 = cdf(Normal(), x)
 
 end  # module

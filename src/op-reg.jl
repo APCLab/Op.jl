c1d4e7d1a3db6ded6b33029051721a66d79fe318
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-15 22:37:04 +0800

    plot

--- src/op-reg.jl
+++ src/op-reg.jl
@@ -72,38 +72,65 @@ trainprovider, evalprovider, plotprovider = input_1()
 # or add/remove a layer
 #= data_ = mx.Variable(:data) =#
 label = mx.Variable(:label)
-net = @mx.chain     mx.Variable(:data) =>
-                    mx.FullyConnected(num_hidden = 5)  =>
-                    mx.Activation(act_type = :relu) =>
-                    mx.FullyConnected(num_hidden = 5)  =>
-                    mx.Activation(act_type = :relu) =>
-                    mx.FullyConnected(num_hidden = 1)  =>
-                    mx.LinearRegressionOutput(label)
+net =
+    @mx.chain mx.Variable(:data) =>
+              mx.FullyConnected(num_hidden = 1024)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 512)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 256)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 128)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 64)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 32)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 16)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 8)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 1)  =>
+              mx.MAERegressionOutput(label)
+
+# net for quick test
+net_qtest =
+    @mx.chain mx.Variable(:data) =>
+              mx.FullyConnected(num_hidden = 10)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 5)  =>
+              mx.Activation(act_type = :relu) =>
+              mx.FullyConnected(num_hidden = 1)  =>
+              mx.MAERegressionOutput(label)
 
 # final model definition, don't change, except if using gpu
-model = mx.FeedForward(net, context=mx.cpu())
+cpus = [mx.cpu(i) for i in 0:3]
+gpu = mx.gpu()
+model = mx.FeedForward(net, context=gpu)
 
 # set up the optimizer: select one, explore parameters, if desired
 #= optimizer = mx.SGD(lr=0.01, momentum=0.9, weight_decay=0.00001) =#
 optimizer = mx.ADAM()
 
 # train, reporting loss for training and evaluation sets
+epoch = 50
 # initial training with small batch size, to get to a good neighborhood
 mx.fit(
-    model, optimizer, initializer=mx.NormalInitializer(0.0,0.1),
-    eval_metric=mx.MSE(), trainprovider, eval_data=evalprovider, n_epoch = 40)
+    model, optimizer, initializer = mx.NormalInitializer(0.0,0.1),
+    eval_metric = mx.NMSE(), trainprovider, eval_data = evalprovider,
+    n_epoch = epoch)
 # more training with the full sample
 mx.fit(
-    model, optimizer, eval_metric=mx.MSE(),
-    trainprovider, eval_data=evalprovider, n_epoch = 40)
+    model, optimizer, eval_metric = mx.NMSE(),
+    trainprovider, eval_data = evalprovider, n_epoch = epoch)
 
-# obtain predictions
-plotprovider = mx.ArrayDataProvider(
-    :data => data_test,
-    :label => target_test)
 fit = mx.predict(model, plotprovider)
-plot(target_test', fit', ".")
-xlabel("real price")
-ylabel("predicted price")
-title("outputs: true versus predicted. 45º line is what we hope for")
-savefig("test.png")
+scatter(target_test', fit')
+#= xlabel("real price") =#
+#= ylabel("predicted price") =#
+#= title("outputs: true versus predicted. 45º line is what we hope for") =#
+savefig("out.png")
+
+result = DataFrame(
+    fit = reshape(fit, length(fit)),
+    real = reshape(target_test, length(target_test)))

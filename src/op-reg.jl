fc84b20e70fca10fd3b5c69ec6073843bd11ab6f
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-15 22:35:13 +0800

    load function

--- src/op-reg.jl
+++ src/op-reg.jl
@@ -6,32 +6,67 @@ using DataFrames
 pyplot()
 
 # data generating process
-cc = readtable("./ccc.csv")
-cc[:mon_σ] *= 1000
+function load(col::Array{Symbol})
+    global cc = readtable("./ccc.csv")
+    cc[:mon_σ] *= 1000
 
-data = convert(Array{Float64}, cc[[:price, :contract, :BS, :mon_σ, :T]])'
-data_idx = Int(round(size(data)[2] * 0.80))
-data_train = data[:, 1:data_idx]
-data_test = data[:, data_idx+1:end]
+    global data = convert(Array{Float64}, cc[col])'
+    global data_idx = Int(round(size(data)[2] * 0.80))
+    global data_train = data[:, 1:data_idx]
+    global data_test = data[:, data_idx+1:end]
 
-target = convert(Array{Float64}, cc[:close])'
-target_idx = Int(round(size(target)[2] * 0.80))
-target_train = target[:, 1:target_idx]
-target_test = target[:, target_idx+1:end]
+    global target = convert(Array{Float64}, cc[:close])'
+    global target_idx = Int(round(size(target)[2] * 0.80))
+    global target_train = target[:, 1:target_idx]
+    global target_test = target[:, target_idx+1:end]
+end
 
-# how to set up data providers using data in memory
-batchsize = 100 # can adjust this later, but must be defined now for next line
-trainprovider = mx.ArrayDataProvider(
-    :data => data_train,
-    batch_size = batchsize,
-    shuffle = true,
-    :label => target_train)
+function input_1()
+    load([:price, :contract, :mon_σ, :T])
 
-evalprovider = mx.ArrayDataProvider(
-    :data => data_test,
-    batch_size = batchsize,
-    shuffle = true,
-    :label => target_test)
+    batchsize = 64
+
+    # WTF: if we enable ``suffle``, the optimizer will fail to converge
+    trainprovider = mx.ArrayDataProvider(
+        :data => data_train,
+        batch_size = batchsize,
+        :label => target_train)
+
+    evalprovider = mx.ArrayDataProvider(
+        :data => data_test,
+        batch_size = batchsize,
+        :label => target_test)
+
+    plotprovider = mx.ArrayDataProvider(
+        :data => data_test,
+        :label => target_test)
+
+    trainprovider, evalprovider, plotprovider
+end
+
+function input_2()  # with TA: MACD
+    load([:price, :contract, :mon_σ, :T])
+
+    batchsize = 64
+
+    trainprovider = mx.ArrayDataProvider(
+        :data => data_train,
+        batch_size = batchsize,
+        :label => target_train)
+
+    evalprovider = mx.ArrayDataProvider(
+        :data => data_test,
+        batch_size = batchsize,
+        :label => target_test)
+
+    plotprovider = mx.ArrayDataProvider(
+        :data => data_test,
+        :label => target_test)
+
+    trainprovider, evalprovider, plotprovider
+end
+
+trainprovider, evalprovider, plotprovider = input_1()
 
 # create a two hidden layer MPL: try varying num_hidden, and change tanh to relu,
 # or add/remove a layer

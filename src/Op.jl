fd8c9a5e1842dd6927cf7ce2fcf6c1386a1a5252
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-23 09:34:19 +0800

    Op: fix model rate

--- src/Op.jl
+++ src/Op.jl
@@ -1,6 +1,6 @@
 module Op
 
-using Distributions: cdf, Normal
+using Distributions: cdf, pdf, Normal
 
 export C
 
@@ -9,18 +9,18 @@ Black-scholes Model
 
 :param S: stock price (Array)
 :param L: strike price
-:param T: time to execute (in year)
+:param T: time to maturity (in years -> 252)
 :param σ: volatility (Array)
-:param r: risk-free rate of interest
+:param r: risk-free rate of interest, it should be continuous rate.
 """
-C(S::AbstractArray, L::AbstractArray, T::Real, σ::AbstractArray,
-  r::Real = 1.04)::Array{Float64} =
-    S .* N(D1(S, L, T, σ, r)) - L .* e^(-r * T) .* N(D2(S, L, T, σ, r))
+C(S::AbstractArray, L::AbstractArray, T::AbstractArray, σ::AbstractArray,
+  r::Real = log(1.04))::Array{Float64} =
+    S .* N(D1(S, L, T, σ, r)) - L .* e.^(-r .* T) .* N(D2(S, L, T, σ, r))
 
 D1(S, L, T, σ, r = 1.04)::Array{Float64} =
-    (log(S ./ L) + (r + 0.5 .* σ.^2) * T) ./ (σ .* √T)
+    (log(S ./ L) .+ (r + 0.5 * σ.^2) .* T) ./ (σ .* √T)
 D2(S, L, T, σ, r = 1.04)::Array{Float64} =
-    (log(S ./ L) + (r - 0.5 .* σ.^2) * T) ./ (σ .* √T)
+    (log(S ./ L) .+ (r - 0.5 * σ.^2) .* T) ./ (σ .* √T)
 
 N(x::Array{Float64})::Array{Float64} = cdf(Normal(), x)
 

0f1555af9229d0bdda934c2352623674ec68fe0e
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-24 21:33:10 +0800

    iter over nets

--- src/op-reg.jl
+++ src/op-reg.jl
@@ -160,88 +160,66 @@ function network_factory()
 end
 
 input(:orig)
+#= input(:ta) =#
 trainprovider, evalprovider, plotprovider = get_provider()
 
-# create a two hidden layer MPL: try varying num_hidden, and change tanh to relu,
-# or add/remove a layer
-#= data_ = mx.Variable(:data) =#
-label = mx.Variable(:label)
-net =
-    @mx.chain mx.Variable(:data) =>
-              mx.FullyConnected(num_hidden = 1024)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 512)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 256)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 128)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 64)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 32)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 16)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 8)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 4)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 2)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 1)  =>
-              mx.LinearRegressionOutput(label)
+#= loss = mx.MakeLoss( =#
+#=     mx.Activation(abs(net .- label) ./ label, act_type=:sigmoid), =#
+#=     grad_scale = 10, =#
+#=     name = :loss, =#
+#=     #= normalization = "batch", =# =#
+#= ) =#
+#= net = mx.Group(mx.BlockGrad(net), loss) =#
 
-# net for quick test
-net_qtest =
-    @mx.chain mx.Variable(:data) =>
-              mx.FullyConnected(num_hidden = 10)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 5)  =>
-              mx.Activation(act_type = :relu) =>
-              mx.FullyConnected(num_hidden = 1)  =>
-              mx.MAERegressionOutput(label)
+# simple small net for quick test
+#= net_qtest = =#
+#=     @mx.chain mx.Variable(:data) => =#
+#=               mx.FullyConnected(num_hidden = 10)  => =#
+#=               mx.Activation(act_type = :relu) => =#
+#=               mx.FullyConnected(num_hidden = 5)  => =#
+#=               mx.Activation(act_type = :relu) => =#
+#=               mx.FullyConnected(num_hidden = 1)  => =#
+#=               mx.MAERegressionOutput(label) =#
 
 # final model definition, don't change, except if using gpu
 cpus = [mx.cpu(i) for i in 0:3]
 gpu = mx.gpu()
-model = mx.FeedForward(net, context=gpu)
 
-# set up the optimizer: select one, explore parameters, if desired
-#= optimizer = mx.SGD(lr=0.01, momentum=0.9, weight_decay=0.00001) =#
-optimizer = mx.ADAM()
+nets = @task network_factory()
+for (net, postfix) ∈ nets
+    model = mx.FeedForward(net, context=gpu)
 
-# train, reporting loss for training and evaluation sets
-epoch = 100
-# initial training with small batch size, to get to a good neighborhood
-mx.fit(
-    model, optimizer,
-    initializer = mx.NormalInitializer(0.0,0.1),
-    eval_metric = mx.NMSE(),
-    trainprovider,
-    eval_data = evalprovider,
-    n_epoch = epoch)
-# more training with the full sample
-mx.fit(
-    model, optimizer,
-    eval_metric = mx.NMSE(),
-    trainprovider,
-    eval_data = evalprovider,
-    n_epoch = epoch)
+    # set up the optimizer: select one, explore parameters, if desired
+    #= optimizer = mx.SGD(lr=0.001, momentum=0.9, weight_decay=0.00001) =#
+    optimizer = mx.ADAM(weight_decay=0.0001)
 
-fit = mx.predict(model, plotprovider)
+    # train, reporting loss for training and evaluation sets
+    epoch = 100
+    # initial training with small batch size, to get to a good neighborhood
+    mx.fit(
+        model, optimizer,
+        initializer = mx.UniformInitializer(0.1),
+        eval_metric = mx.MSE(),
+        trainprovider,
+        eval_data = evalprovider,
+        n_epoch = epoch)
 
-function plot_pred(target_test, fit)
-    scatter(target_test', fit',
-            xlim = (0, 600), ylim = (0, 600),
-            xlabel = "real price", ylabel = "pred price",
-            title = plot_title)
-    savefig("out.png")
-    write("net.dot", mx.to_graphviz(net))
-    run(`dot -Tpng -o net.png net.dot`)
+    fit = mx.predict(model, plotprovider)
+    #= plot_pred(target_test, fit; name="out-pre.png") =#
+
+    # more training with the full sample
+    mx.fit(
+        model, optimizer,
+        eval_metric = mx.MSE(),
+        trainprovider,
+        eval_data = evalprovider,
+        n_epoch = epoch)
+
+    fit = mx.predict(model, plotprovider)
+
+    plot_pred(target_test, fit, net, postfix)
+
+    result = DataFrame(
+        fit = reshape(fit, length(fit)),
+        real = reshape(target_test, length(target_test)))
 end
-
-plot_pred(target_test, fit)
-
-result = DataFrame(
-    fit = reshape(fit, length(fit)),
-    real = reshape(target_test, length(target_test)))

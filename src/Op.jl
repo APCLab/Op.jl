9218fc7c3c9d6e3322721cfa17b2e259bbbe20e7
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-05-04 09:32:04 +0800

    C: round too small value to 0.1

--- src/Op.jl
+++ src/Op.jl
@@ -12,10 +12,22 @@ Black-scholes Model
 :param T: time to maturity (in years -> 252)
 :param σ: volatility (Array)
 :param r: risk-free rate of interest, it should be continuous rate.
+
+:param round: round small value to ``0.1``, default is ``false``
 """
-C(S::AbstractArray, L::AbstractArray, T::AbstractArray, σ::AbstractArray,
-  r::Real = log(1.04))::Array{Float64} =
-    S .* N(D1(S, L, T, σ, r)) - L .* e.^(-r .* T) .* N(D2(S, L, T, σ, r))
+function C(S::AbstractArray, L::AbstractArray, T::AbstractArray,
+           σ::AbstractArray, r::Real = log(1.04);
+           round=false)::Array{Float64}
+    c = S .* N(D1(S, L, T, σ, r)) - L .* e.^(-r .* T) .* N(D2(S, L, T, σ, r))
+
+    if round == false
+        map(c) do x
+            (x <= 0.1) ? 0.1 : x
+        end
+    else
+        c
+    end
+end
 
 D1(S, L, T, σ, r = 1.04)::Array{Float64} =
     (log(S ./ L) .+ (r + 0.5 * σ.^2) .* T) ./ (σ .* √T)
